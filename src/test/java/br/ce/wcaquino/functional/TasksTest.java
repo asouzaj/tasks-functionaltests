aac410ed05d9c685be9ae8cfa091cfad6fa93e5b
ademarjoaquim <ajoaquim@yahoo.com>
2021-09-23 15:59:11 -0300

    paralelismo 2

--- src/test/java/br/ce/wcaquino/functional/TasksTest.java
+++ src/test/java/br/ce/wcaquino/functional/TasksTest.java
@@ -27,7 +27,7 @@ public class TasksTest {
 		
 		//carregar a URL para testar funcional
 		//carregar o ipconfig e não localhost
-		driver.navigate().to("http://192.168.100.81:8001/tasks");
+		driver.navigate().to("http://192.168.0.6:8001/tasks");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
 		return driver;
@@ -58,7 +58,7 @@ public class TasksTest {
 			//clicar botao salvar
 			driver.findElement(By.id("saveButton")).click();
 
-			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			
 
 			//validar a inclusao message Success!!!
 			String message = driver.findElement(By.id("message")).getText();
